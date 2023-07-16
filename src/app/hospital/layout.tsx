12377e5706ac97b7027aea4117a571db90408cd2
breezjin <breezjin@gmail.com>
2023-07-17 00:01:52 +0900

    comming soon

--- src/app/hospital/layout.tsx
+++ src/app/hospital/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
             <div className='flex w-full flex-col'>
+              <SiteHeader />
               <section className=''>{children}</section>
             </div>
             <TailwindIndicator />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex w-full min-h-screen flex-col items-center justify-center p-24'>
-      나란히 정신건강의학과의원
+      <div className='text-3xl'>나란히</div>
+      <div className='text-3xl'>정신건강의학과의원</div>
+      <div className='text-3xl'>& 심리발달센터</div>
+      <div className='m-12 text-3xl'><span className='text-yellow-600'>9월</span>에 오픈 합니다.</div>
+      <div className=''>서울특별시 동작구 현충로 75, 3층 2-1호</div>
+      <div className=''>원불교100년기념관 및 역사문화기념관</div>
+      
     </main>
   );
 }
